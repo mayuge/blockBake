7ccf4a4448efdcaebfee487ed3bb9bf1c7e13ba0
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2026-02-19 22:40:51 +0900

    feat:設定ファイル等の環境整備

--- 03_prototype/block-bake-game/vite.config.ts
+++ 03_prototype/block-bake-game/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
+import { fileURLToPath } from "node:url"
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin"
+import { playwright } from "@vitest/browser-playwright"
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  test: {
+    // デフォルトでユニットテストのみを収集する
+    include: ["**/__tests__/*.test.ts"],
+    exclude: ["**/*.stories.*", "**/*.story.*", "src/**/*.mdx"],
+  },
 })
